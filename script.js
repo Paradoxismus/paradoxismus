e0218b4926e8c9a27a00e2e9c2e9860e8fb0b10a
Paradoxismus <100711326+Paradoxismus@users.noreply.github.com>
2026-01-14 21:10:21 +0100

    Temperature Guesser
    Guess the temperature.

--- script.js
+++
@@ -1,6 +0,0 @@
-function randomColor() {
-    let colors = ["red", "blue", "green", "purple", "orange", "pink", "yellow"];
-    document.getElementById("text-wechsler1").style.color =
-      colors[Math.floor(Math.random() * colors.length)];
-  }
-  setInterval(randomColor, 1000);
